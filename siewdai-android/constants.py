65f500fce28841980a974d2fb4f28d5570c3af46
Nicholas Ch <nicholaschongwb@gmail.com>
2025-03-14 17:21:24 +0800

    chore: update constants

--- siewdai-android/constants.py
+++ siewdai-android/constants.py
@@ -4,6 +4,9 @@ PROJ_PATH = os.path.dirname(os.path.realpath(__file__))
 APK_PATH = f"{os.path.abspath(os.path.join(PROJ_PATH, os.pardir))}/apk"
 RESOURCES_PATH = f"{os.path.abspath(os.path.join(PROJ_PATH, os.pardir))}/resources"
 OUTPUT_PATH = f"{os.path.abspath(os.path.join(PROJ_PATH, os.pardir))}/output"
+OUTPUT_APKTOOL_PATH = f"{OUTPUT_PATH}/apktool"
+OUTPUT_JADX_PATH = f"{OUTPUT_PATH}/jadx"
+
 
 APKTOOL_JAR = RESOURCES_PATH + '/apktool/apktool_2.11.1.jar'
 APKTOOL_BINARY = RESOURCES_PATH + '/apktool/apktool'
